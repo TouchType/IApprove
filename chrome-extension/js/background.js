9657b53cbe075af7456fc3bfa91d85982047f884
Chetan Padia <chetan@swiftkey.net>
2013-12-04 15:41:01 +0000

    reconect chrome extension if disconnected

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,3 +1,5 @@
+var WS_URL = "ws://localhost:9090/who-approves";
+
 function notifyApproval(params) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		if (tabs && tabs[0]) {
@@ -6,22 +8,46 @@ function notifyApproval(params) {
 	});
 }
 
-var ws = $.gracefulWebSocket("ws://localhost:9090/who-approves");
+function handle_message(k,v) {
+	console.log("message:", k, v);
+	if (k === "approves") {
+		notifyApproval(v);
+	} else {
+		console.error("Unhandled message:", k, v);
+	}
+}
 
-ws.onmessage = function(e) {
-	$.each(JSON.parse(e.data), function(k,v) {
-		console.log("message:", k, v);
-		if (k === "approves") {
-			notifyApproval(v);
-		} else {
-			console.error("Unhandled request:", k, v);
+var message_queue = [];
+var ws = null;
+function connect() {
+	console.log("Connecting to " + WS_URL);
+	ws = $.gracefulWebSocket(WS_URL);
+	ws.onmessage = function(e) {
+		$.each(JSON.parse(e.data), handle_message);
+	};
+	ws.onopen = function() {
+		console.log("Connected.");
+		while(message_queue.length) {
+			ws.send(message_queue[0]);
+			message_queue.shift();
 		}
-	});
-};
+	};
+	ws.onclose = function() {
+		console.warn("Connection closed.");
+		setTimeout(connect, 4000);
+	};
+}
+connect();
 
 chrome.tabs.onActivated.addListener(function(tab_info) {
 	chrome.tabs.get(tab_info.tabId, function(t) {
 		var tab = {url: t.url, title: t.title};
-		ws.send(JSON.stringify({"tab-changed": tab}));
+		var message = JSON.stringify({"tab-changed": tab});
+		if (ws) {
+			ws.send(message)
+		} else {
+			console.warn("Not connected. Sending later.");
+			message_queue.push(message);
+		};
 	});
 });
