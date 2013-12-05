f433732ae0f6f4e3f06fabc71260b23581c38791
Chetan Padia <chetan@swiftkey.net>
2013-12-05 00:31:41 +0000

    watch for changes in URL and switched between windows

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,6 +1,6 @@
 var WS_URL = "ws://localhost:9090/who-approves";
 
-function notifyApproval(params) {
+function notify_approval(params) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		if (tabs && tabs[0]) {
 			chrome.tabs.sendMessage(tabs[0].id, params);
@@ -11,7 +11,7 @@ function notifyApproval(params) {
 function handle_message(k,v) {
 	console.log("message:", k, v);
 	if (k === "approves") {
-		notifyApproval(v);
+		notify_approval(v);
 	} else {
 		console.error("Unhandled message:", k, v);
 	}
@@ -40,9 +40,18 @@ function connect() {
 }
 connect();
 
-chrome.tabs.onActivated.addListener(function(tab_info) {
-	chrome.tabs.get(tab_info.tabId, function(t) {
+function handle_tab_change(tab_id) {
+	if (tab_id === undefined) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tab_info) {
+			console.log(tab_info);
+			if (tab_info) handle_tab_change(tab_info[0].id);
+		});
+		return;
+	}
+
+	chrome.tabs.get(tab_id, function(t) {
 		var tab = {url: t.url, title: t.title};
+		console.log('active page changed: ', tab);
 		var message = JSON.stringify({"tab-changed": tab});
 		if (ws) {
 			ws.send(message)
@@ -51,4 +60,22 @@ chrome.tabs.onActivated.addListener(function(tab_info) {
 			message_queue.push(message);
 		};
 	});
+}
+
+// tell the server what the current tab is
+handle_tab_change();
+
+// watch for switches between tabs
+chrome.tabs.onActivated.addListener(function(tab_info) {
+	handle_tab_change(tab_info.tabId);
+});
+
+// watch for tab updates
+chrome.tabs.onUpdated.addListener(function(tab_info) {
+	handle_tab_change(tab_info.tabId);
+});
+
+// watch for window changes
+chrome.windows.onFocusChanged.addListener(function() {
+	handle_tab_change();
 });
